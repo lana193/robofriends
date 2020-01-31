c39771cc5e1c5d6de85b8b953be5ead1cd168d6e
Svitlana Kryzhanivska <lana19333@gmail.com>
2020-01-31 17:58:59 +0200

    Updates

---
+++ precache-manifest.1b706d91e7e2b2918dfb34452d91901d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e84673dd9fb4e336d2b5bd4e63bc8e6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "aecec298befed8c70608",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0504e25e20d01ca59a19",
+    "url": "/robofriends/static/css/main.81510d71.chunk.css"
+  },
+  {
+    "revision": "aecec298befed8c70608",
+    "url": "/robofriends/static/js/2.e87b60c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.e87b60c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0504e25e20d01ca59a19",
+    "url": "/robofriends/static/js/main.982ed92b.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "4e04bcb7a0a4aaea66d1196d0a147e40",
+    "url": "/robofriends/static/media/Arsenal 15 16 vh.4e04bcb7.woff"
+  }
+]);
